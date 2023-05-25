f2905060cb1fc174c11e86e069007e37b4f874e6
KongRuksiam Official <kongruksiamstudio@gmail.com>
2023-05-25 18:32:44 +0700

    Add files via upload

---
+++ JS-DataStructure (Non-Linear)/Trees/BST-Constructor.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+let myBST = new BinarySearchTree();
+console.log(myBST);
